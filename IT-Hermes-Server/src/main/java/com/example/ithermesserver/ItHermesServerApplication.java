52aeeaa79dd3b2d8475b744adfc3a1897384b529
eunyoung23 <eun970923@naver.com>
2024-06-10 21:49:01 +0900

    add project file

---
+++ IT-Hermes-Server/src/main/java/com/example/ithermesserver/ItHermesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ithermesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHermesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItHermesServerApplication.class, args);
+	}
+
+}
---
+++ IT-Hermes-Server/src/test/java/com/example/ithermesserver/ItHermesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ithermesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItHermesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
